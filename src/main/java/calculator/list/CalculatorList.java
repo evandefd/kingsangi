4e7264bc5c23f44b786048c22ed2dba5331ee2a6
Seungmin Yang <tmdals099@gmail.com>
2020-12-14 23:09:45 +0900

    ArrayList를 LinkedList로 변경
    데이터의 검색이 이루어지지 않는다고 판단, 데이터 추가 시 불필요한 데이터 복사를 방지하기 위함

--- src/main/java/calculator/list/CalculatorList.java
+++ src/main/java/calculator/list/CalculatorList.java
@@ -3,8 +3,11 @@ package calculator.list;
 import calculator.enums.Operator;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class CalculatorList extends ArrayList<Object> {
+public class CalculatorList extends LinkedList<Object> {
+    private static final long serialVersionUID = 3182418425156988317L;
+
     @Override
     public boolean add(Object o) {
         if(o instanceof Number || o instanceof Operator) {
