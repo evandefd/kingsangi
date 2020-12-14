609e2109142bd9c01c38d7445ddc0a1bd3cd6e89
Seungmin Yang <tmdals099@gmail.com>
2020-12-14 23:37:36 +0900

    Delimeter  제거

--- src/main/java/calculator/enums/Operator.java
+++ src/main/java/calculator/enums/Operator.java
@@ -6,36 +6,36 @@ package calculator.enums;
  * @author Seungmin Yang
  */
 public enum Operator {
-    ADD("+", '+'),
-    SUB("-", '-'),
-    MUL("×", '*'),
-    DIV("÷", '/'),
-    POW("^", '^'),
-    SQRT("\u221A", 'r'),
-    FACT("!", '!'),
-    SIN("sin", 's'),
-    COS("cos", 'c'),
-    TAN("tan", 't'),
-    ASIN("sin\u207b\u00b9", 'S'),
-    ACOS("cos\u207b\u00b9", 'C'),
-    ATAN("tan\u207b\u00b9", 'T'),
-    LOG("log", 'l'),
-    LN("ln", 'L'),
-    PER("%", '%');
+    ADD("+", 1),
+    SUB("-", 1),
+    MUL("×", 2),
+    DIV("÷", 2),
+    POW("^", 3),
+    SQRT("\u221A", 3),
+    FACT("!", 3),
+    SIN("sin", 3),
+    COS("cos", 3),
+    TAN("tan", 3),
+    ASIN("sin\u207b\u00b9", 3),
+    ACOS("cos\u207b\u00b9", 3),
+    ATAN("tan\u207b\u00b9", 3),
+    LOG("log", 3),
+    LN("ln", 3),
+    PER("%", 3);
 
     private String display;
-    private char delimiter;
+    private int priority;
 
-    Operator(String display, char delimiter) {
+    Operator(String display, int priority) {
         this.display = display;
-        this.delimiter = delimiter;
+        this.priority = priority;
     }
 
     public String getDisplayString() {
         return display;
     }
 
-    public char getDelimiter() {
-        return delimiter;
+    public int getPriority() {
+        return priority;
     }
 }
