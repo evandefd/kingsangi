b331166753c8cb0860265099d7c53925f805bce2
한성현 <han990102@naver.com>
2020-12-22 11:02:29 +0900

    음력을 양력으로 바꾸는 함수추가
    음력을 양력으로 바꾸는 함수 chineseDateToDate추가 및 java파일의 package 수정

--- src/main/java/datecalculator/DateToChineseDate.java
+++ src/main/java/datecalculator/DateToChineseDate.java
@@ -1,3 +1,5 @@
+package datecalculator;
+
 import com.ibm.icu.util.ChineseCalendar;
 
 import java.text.DateFormat;
@@ -6,19 +8,21 @@ import java.util.Calendar;
 
 public class DateToChineseDate {
 
+    public static final int different = 2637; //음력과 양력간 년수 차이
+
     public static String dateToChineseDate(long millisecond){
         DateFormat dfm = SimpleDateFormat.getDateInstance(DateFormat.DEFAULT);
 
         Calendar cal = Calendar.getInstance();
         ChineseCalendar chineseCalendar = new ChineseCalendar();
 
-        cal.setTimeInMillis(millisecond);
+        cal.setTimeInMillis(millisecond); //날짜 설정
 
-        chineseCalendar.setTimeInMillis(cal.getTimeInMillis());
+        chineseCalendar.setTimeInMillis(cal.getTimeInMillis()); //chineseCalendar 에 날짜설정
 
-        int year= chineseCalendar.get(ChineseCalendar.EXTENDED_YEAR)-2637;
-        int month = chineseCalendar.get(ChineseCalendar.MONTH);
-        int day = chineseCalendar.get(ChineseCalendar.DAY_OF_MONTH);
+        int year= chineseCalendar.get(ChineseCalendar.EXTENDED_YEAR)-different; //chineseCalendar 에서 년도 추줄
+        int month = chineseCalendar.get(ChineseCalendar.MONTH); //chineseCalendar 에서 월 추줄
+        int day = chineseCalendar.get(ChineseCalendar.DAY_OF_MONTH); //chineseCalendar 일 추줄
 
         cal.set(year,month,day);
 
@@ -26,4 +30,26 @@ public class DateToChineseDate {
 
         return str;
     }
+
+    public static String chineseDateToDate (long millisecond){ //음력을 양력으로 변환
+        DateFormat dfm = SimpleDateFormat.getDateInstance(DateFormat.DEFAULT);
+
+        Calendar cal = Calendar.getInstance();
+        ChineseCalendar chineseCalendar = new ChineseCalendar();
+
+        cal.setTimeInMillis(millisecond);
+
+        chineseCalendar.set(ChineseCalendar.EXTENDED_YEAR,cal.get(Calendar.YEAR)+different);
+        chineseCalendar.set(ChineseCalendar.MONTH,cal.get(Calendar.MONTH));
+        chineseCalendar.set(ChineseCalendar.DAY_OF_MONTH,cal.get(Calendar.DAY_OF_MONTH));
+
+        cal.setTimeInMillis(chineseCalendar.getTimeInMillis());
+
+        String str = dfm.format(cal.getTime());
+
+        return str;
+
+    }
 }
+
+
