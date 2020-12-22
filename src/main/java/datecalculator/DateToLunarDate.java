28728870f6d9f664bdb163ada2059a7561ed4bd6
한성현 <han990102@naver.com>
2020-12-22 11:41:23 +0900

    2차수정
    음력 양력 변환 클래스 및 함수명 수정

---
+++ src/main/java/datecalculator/DateToLunarDate.java
@@ -0,0 +1,54 @@
+package datecalculator;
+
+import com.ibm.icu.util.ChineseCalendar;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DateToLunarDate {
+
+    public static final int different = 2637; //음력과 양력간 년수 차이
+
+    public static String dateToLunarDate(long millisecond){
+        DateFormat dfm = SimpleDateFormat.getDateInstance(DateFormat.DEFAULT);
+
+        Calendar cal = Calendar.getInstance();
+        ChineseCalendar chineseCalendar = new ChineseCalendar();
+
+        cal.setTimeInMillis(millisecond);
+
+        chineseCalendar.setTimeInMillis(cal.getTimeInMillis());
+
+        int year= chineseCalendar.get(ChineseCalendar.EXTENDED_YEAR)-different;
+        int month = chineseCalendar.get(ChineseCalendar.MONTH);
+        int day = chineseCalendar.get(ChineseCalendar.DAY_OF_MONTH);
+
+        cal.set(year,month,day);
+
+        String str = dfm.format(cal.getTime());
+
+        return str;
+    }
+
+    public static String lunarDateToDate (long millisecond){ //음력을 양력으로 변환
+        DateFormat dfm = SimpleDateFormat.getDateInstance(DateFormat.DEFAULT);
+
+        Calendar cal = Calendar.getInstance();
+        ChineseCalendar chineseCalendar = new ChineseCalendar();
+
+        cal.setTimeInMillis(millisecond);
+
+        chineseCalendar.set(ChineseCalendar.EXTENDED_YEAR,cal.get(Calendar.YEAR)+different);
+        chineseCalendar.set(ChineseCalendar.MONTH,cal.get(Calendar.MONTH));
+        chineseCalendar.set(ChineseCalendar.DAY_OF_MONTH,cal.get(Calendar.DAY_OF_MONTH));
+
+        cal.setTimeInMillis(chineseCalendar.getTimeInMillis());
+
+        String str = dfm.format(cal.getTime());
+
+        return str;
+
+    }
+}
+}
--- src/main/java/datecalculator/TodayAddDay.java
+++ src/main/java/datecalculator/TodayAddDay.java
@@ -1,4 +1,4 @@
-package main.java;
+package datecalculator;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
